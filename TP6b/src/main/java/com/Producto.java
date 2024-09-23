bdf7f3b5eee6e034e278746b3b0eb19a0de48267
Ulises Perez <104675087+odyssey07@users.noreply.github.com>
2024-09-22 22:48:05 -0300

    Add files via upload

---
+++ TP6b/src/main/java/com/Producto.java
@@ -0,0 +1,76 @@
+package com;
+
+import java.util.Objects;
+
+public class Producto implements Comparable <Producto> {  
+    private int codigo;
+    private String descripcion;
+    private Double precio;
+    private int stock;
+    private String rubro; // comestible limpieza perfumeria 
+
+    public Producto(int codigo, String descripcion, Double precio, int stock, String rubro) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.rubro = rubro;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+    
+    
+
+    @Override
+    public int compareTo(Producto o) {
+        return Integer.valueOf(codigo).compareTo(o.codigo);
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "codigo=" + codigo + ", descripcion=" + descripcion + ", precio=" + precio + ", stock=" + stock + ", rubro=" + rubro + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Producto other = (Producto) obj;
+        if (this.codigo != other.codigo) {
+            return false;
+        }
+        return Objects.equals(this.descripcion, other.descripcion);
+    }
+}
+
